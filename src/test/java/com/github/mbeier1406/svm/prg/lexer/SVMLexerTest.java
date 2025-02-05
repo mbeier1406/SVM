2a3b482e6a5f83adf033af9b327c8e8feea0e51e
Martin Beier <Martin.Beier@gmx.de>
2025-02-05 15:24:03 -0500

    SVM - Simple Virtual Machine: Lexer

--- src/test/java/com/github/mbeier1406/svm/prg/lexer/SVMLexerTest.java
+++ src/test/java/com/github/mbeier1406/svm/prg/lexer/SVMLexerTest.java
@@ -2,9 +2,11 @@ package com.github.mbeier1406.svm.prg.lexer;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.apache.logging.log4j.LogManager;
@@ -25,44 +27,79 @@ public class SVMLexerTest {
 		assertThat(ex.getLocalizedMessage(), containsString("token"));
 	}
 
-	/** Stellt sicher, dass ein fehlender Wert in {@linkplain SVMLexer.Symbol} einen leeren {@linkplain Optional} als String liefert */
+	/** Stellt sicher, dass ein fehlender <i>Value</i> in {@linkplain SVMLexer.Symbol} die korrekten Werte liefert liefert */
 	@Test
-	public void testeSymbolValueStringNullValue() {
-		assertThat(new SVMLexer.Symbol(SVMLexer.Token.COMMA, null).getStringValue(), equalTo(Optional.empty()));
+	public void testeSymbolNullValue() {
+		var symbol = new SVMLexer.Symbol(SVMLexer.Token.COMMA, null);
+		assertAll("Symbol COMA",
+				() -> assertEquals(symbol.getIntValue(), Optional.empty()),
+				() -> assertEquals(symbol.getStringValue(), Optional.empty()),
+				() -> assertEquals(symbol.value(), null),
+				() -> assertEquals(symbol.token(), SVMLexer.Token.COMMA));
 	}
 
-	/** Stellt sicher, dass ein fehlender Wert in {@linkplain SVMLexer.Symbol} einen leeren {@linkplain Optional} als Integer liefert */
+	/** Stellt sicher, dass ein <i>Text-Value</i> in {@linkplain SVMLexer.Symbol} die korrekten Werte liefert liefert */
 	@Test
-	public void testeSymbolValueIntNullValue() {
-		assertThat(new SVMLexer.Symbol(SVMLexer.Token.COMMA, null).getIntValue(), equalTo(Optional.empty()));
+	public void testeSymbolTextValue() {
+		var symbol = new SVMLexer.Symbol(SVMLexer.Token.CONSTANT, "wert");
+		assertAll("Symbol CONSTANT",
+				() -> assertEquals(symbol.getStringValue(), Optional.of("wert")),
+				() -> assertEquals(symbol.value(), "wert"),
+				() -> assertEquals(symbol.token(), SVMLexer.Token.CONSTANT));
 	}
 
-	/** Stellt sicher, dass ein Text-{@linkplain SVMLexer.Symbol} den korrekten Wert liefert */
-	@Test
-	public void testeSymbolStringValue() {
-		assertThat(new SVMLexer.Symbol(SVMLexer.Token.CONSTANT, "Test").getStringValue(), equalTo(Optional.of("Test")));
-	}
-
-	/** Stellt sicher, dass ein fehlender {@linkplain SVMLexer.Token} in {@linkplain SVMLexer.Symbol} einen definierten Fehler liefert */
+	/** Stellt sicher, dass ein <i>Text-Value</i> in {@linkplain SVMLexer.Symbol} als Zahl abgerufen einen definierten Fehler liefert */
 	@Test
 	public void testeSymbolStringValueGetInt() {
 		var ex = assertThrows(NumberFormatException.class, () -> new SVMLexer.Symbol(SVMLexer.Token.CONSTANT, "text").getIntValue());
 		assertThat(ex.getLocalizedMessage(), containsString("text"));
 	}
 
+	/** Stellt sicher, dass ein <i>Number-Value</i> in {@linkplain SVMLexer.Symbol} die korrekten Werte liefert liefert */
 	@Test
-	public void testeSymbolLineNumberNull() {
-		
+	public void testeSymbolNumberValue() {
+		var symbol = new SVMLexer.Symbol(SVMLexer.Token.CONSTANT, "1234");
+		assertAll("Symbol CONSTANT",
+				() -> assertEquals(symbol.getIntValue(), Optional.of(1234)),
+				() -> assertEquals(symbol.getStringValue(), Optional.of("1234")),
+				() -> assertEquals(symbol.value(), "1234"),
+				() -> assertEquals(symbol.token(), SVMLexer.Token.CONSTANT));
 	}
 
+	/** Stellt sicher, dass eine fehlende Zeile in {@linkplain SVMLexer.LineInfo} einen definierten Fehler liefert */
 	@Test
-	public void testeSymbolLineNullValue() {
-		
+	public void testeLineInfoStringNull() {
+		var ex = assertThrows(NullPointerException.class, () -> new SVMLexer.LineInfo(1, null, new ArrayList<>()));
+		assertThat(ex.getLocalizedMessage(), containsString("line"));
 	}
 
+	/** Stellt sicher, dass fehlende Symbole in {@linkplain SVMLexer.LineInfo} einen definierten Fehler liefert */
 	@Test
-	public void testeSymbolLineLineNumberNull() {
-		
+	public void testeLineInfoSymbolsNull() {
+		var ex = assertThrows(NullPointerException.class, () -> new SVMLexer.LineInfo(1, "line", null));
+		assertThat(ex.getLocalizedMessage(), containsString("symbols"));
+	}
+
+	/** Stellt sicher, dass eine ungültige Zeilennummer in {@linkplain SVMLexer.LineInfo} einen definierten Fehler liefert */
+	@Test
+	public void testeLineInfoLineNumber() {
+		var ex = assertThrows(IllegalArgumentException.class, () -> new SVMLexer.LineInfo(-1, "line", new ArrayList<>()));
+		assertThat(ex.getLocalizedMessage(), containsString("-1"));
+	}
+
+	/** Stellt sicher, dass gültige {@linkplain SVMLexer.LineInfo} korrekte Werte liefert */
+	@Test
+	@SuppressWarnings("serial")
+	public void testeLineInfo() {
+		var symbols = new ArrayList<SVMLexer.Symbol>() {{
+			add(new SVMLexer.Symbol(SVMLexer.Token.CONSTANT, "wert"));
+			add(new SVMLexer.Symbol(SVMLexer.Token.COMMA, null));
+		}};
+		var lineInfo = new SVMLexer.LineInfo(5, "line 5", symbols);
+		assertAll("LineInfo",
+				() -> assertEquals(lineInfo.symbols(), symbols),
+				() -> assertEquals(lineInfo.line(), "line 5"),
+				() -> assertEquals(lineInfo.lineNumber(), 5));
 	}
 
 }

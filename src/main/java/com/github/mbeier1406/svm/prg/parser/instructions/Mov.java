7050e38bd12c68643a839f2f3859e77361d23a7a
Martin Beier <Martin.Beier@gmx.de>
2025-02-27 22:12:58 +0100

    SVM - Simple Virtual Machine: Parser

--- src/main/java/com/github/mbeier1406/svm/prg/parser/instructions/Mov.java
+++ src/main/java/com/github/mbeier1406/svm/prg/parser/instructions/Mov.java
@@ -3,7 +3,9 @@ package com.github.mbeier1406.svm.prg.parser.instructions;
 import static com.github.mbeier1406.svm.prg.parser.Helper.checkParameterToken;
 import static java.util.Objects.requireNonNull;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.github.mbeier1406.svm.SVMException;
@@ -17,32 +19,52 @@ import com.github.mbeier1406.svm.prg.lexer.SVMLexer.LineInfo;
 import com.github.mbeier1406.svm.prg.lexer.SVMLexer.Symbol;
 import com.github.mbeier1406.svm.prg.lexer.SVMLexer.Token;
 import com.github.mbeier1406.svm.prg.parser.Helper;
+import com.github.mbeier1406.svm.prg.parser.functions.FunctionFactory;
 
 /**
  * Erzeugt aus einer vom {@linkplain SVMLexer} eingelesenen Codezeile
  * mit einer Instruktion {@linkplain com.github.mbeier1406.svm.instructions.Mov}
- * die zugehörige virtuelle Instruktion. Beispiel:
+ * die zugehörige virtuelle Instruktion.<p/>
+ * Beispiel Syntax 1 (ohne Funktion):
  * <pre><code>
  * .label1	# Optional
  * 	mov $1, %1
  * </code></pre><p/>
- * Syntax: {@code mov (<Number>|<Register>|<Label-Reference>|<Function><Label-Reference>) , (<Register>|<Register-Reference>)}
+ * Beispiel Syntax 2 (mit Funktion):
+ * <pre><code>
+ * 	&data
+ * .label1
+ * 	abc
+ * 	&code
+ * 	mov len(label1), %1
+ * </code></pre><p/>
+ * Syntax: {@code mov ((<Number>|<Register>)|(<Function>\(<Label-Reference>\)) , (<Register>|<Register-Reference>)}
  * @see {@linkplain InstructionFactory#INSTRUCTIONS}
  * @see com.github.mbeier1406.svm.instructions.Mov
  */
 public class Mov implements InstructionParser<Short> {
 
-	/** Liste der Token/Typen, die als jeweiliger Parameter zulässig sind */
-	public static final Token[][] PARAMS = new Token[][] {
-		new Token[] { Token.CONSTANT, Token.FUNCTION, Token.LABEL_REF, Token.REGISTER },
+	/** Liste der <u>drei</u> Token/Typen, die als jeweiliger Parameter zulässig sind bei Syntax 1 (ohne Funktion) */
+	public static final Token[][] PARAMS4 = new Token[][] {
+		new Token[] { Token.CONSTANT, Token.REGISTER },
 		new Token[] { Token.COMMA },
-		new Token[] { Token.REGISTER }
+		new Token[] { Token.REGISTER } // später auch Registerreferenz
+	};
+
+	/** Liste der <u>sechs</u> Token/Typen, die als jeweiliger Parameter zulässig sind bei Syntax 2 (mit Funktion) */
+	public static final Token[][] PARAMS7 = new Token[][] {
+		new Token[] { Token.FUNCTION },
+		new Token[] { Token.LEFTPAR },
+		new Token[] { Token.LABEL_REF },
+		new Token[] { Token.RIGHTPAR },
+		new Token[] { Token.COMMA },
+		new Token[] { Token.REGISTER } // später auch Registerreferenz
 	};
 
 	/**
 	 * Der <u>erste</u> Parameter zur MOV-Instruktion ist ein Byte, dass angibt, von wo
 	 * nach wo verschoben werden soll. Beispiel <code>21</code> gibt an, dass eine
-	 * Konstante in ein register verschoben werdne soll. Als SVM-Programm Beispiel:
+	 * Konstante in ein Register verschoben werden soll. Als SVM-Programm Beispiel:
 	 * {@code mov $2 %1}
 	 * @see com.github.mbeier1406.svm.instructions.Mov
 	 */
@@ -57,16 +79,37 @@ public class Mov implements InstructionParser<Short> {
 	/** {@inheritDoc} */
 	@Override
 	public VirtualInstruction<Short> getVirtualInstruction(final Symbol label, final LineInfo lineInfo, final SVMProgram<Short> svmProgram) throws SVMException {
-		if ( requireNonNull(lineInfo, "lineInfo").symbols().size() != 4 ) // Syntax siehe oben
-			throw new SVMException("MOV erwartet drei Parameter: "+lineInfo.symbols());
+		int numSymbols = requireNonNull(lineInfo, "lineInfo").symbols().size();
+		if ( numSymbols != 4 && numSymbols != 7 ) // Syntax siehe oben
+			throw new SVMException("MOV erwartet drei bzw. fünf Parameter: "+lineInfo.symbols());
 		if ( !lineInfo.symbols().get(0).equals(SVMLexer.SYM_MOV) )
 			throw new SVMException("MOV erwartet Symbol "+SVMLexer.SYM_MOV);
+		boolean syntax1 = numSymbols == 4; // Syntax 1 mov x , y ohne Funktion
+		Token[][] PARAMS = syntax1 ? PARAMS4 : PARAMS7;
 		for ( int i = 0; i < PARAMS.length; i++ )
 			checkParameterToken(i+1, lineInfo.symbols().get(i+1).token(), PARAMS[i]);
 
+		final List<Symbol> s = new ArrayList<>(); // Bei Funktionen muss die zugehörige Konstante (Zahl) berechnet werden
+		if ( !syntax1 ) {
+			// Es wird eine Funktion benutzt, befindet sich an Index 1 (mov len(label)...), die Labelreferenz an Index 3
+			int wert = FunctionFactory.getFunction(lineInfo.symbols().get(1).value()).apply(lineInfo.symbols().get(3), svmProgram);
+			s.add(new Symbol(Token.CONSTANT, String.valueOf(wert)));
+		}
+
+		/* Nachdem die Funktion aufgelöst wurde, Symbolliste vereinheitlichen */
+		@SuppressWarnings("serial")
+		List<Symbol> symbols = syntax1 ?
+				lineInfo.symbols() :
+					new ArrayList<Symbol>() {{
+						add(lineInfo.symbols().get(0)); // mov
+						add(s.get(0)); // die oben berechnete Konstante
+						add(lineInfo.symbols().get(5)); // ,
+						add(lineInfo.symbols().get(6)); // mov
+					}};
+
 		/* Die Parameterliste für die MOV-Instruktion */
-		Symbol param1 = lineInfo.symbols().get(1); // Beispiel MOV $1, %2 wäre hier das zweite Symbol $1 (erste Symbol ist MOV)
-		Symbol param2 = lineInfo.symbols().get(3); // Beispiel MOV $1, %2 wäre hier das vierte Symbol %2 (drittes Symbol ist ,)
+		Symbol param1 = symbols.get(1); // Beispiel MOV $1, %2 wäre hier das zweite Symbol $1 (erste Symbol ist MOV)
+		Symbol param2 = symbols.get(3); // Beispiel MOV $1, %2 wäre hier das vierte Symbol %2 (drittes Symbol ist ,)
 		byte[] params = new byte[5]; // Die fünf Parameter zur Instruktion MOV
 		params[0] = (byte) (requireNonNull(PARAM1_MAP.get(param1.token()), "Kein Wert param1") * 16 +
 				requireNonNull(PARAM1_MAP.get(param2.token()), "Kein Wert param2")); // 1. von wo nach wo
